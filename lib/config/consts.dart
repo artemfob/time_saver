f81fff17c9fee00f9f35f363a95142d091e52797
kislyy <a.kislyy@mobidev.biz>
2024-03-25 13:36:26 +0200

    fix url and questions

--- lib/config/consts.dart
+++ lib/config/consts.dart
@@ -4,234 +4,228 @@ class Constants_ {
   static const String showWebView = 'black';
   static const String packageNameAndroid = 'com.quiz.tiger';
   static const String requestUrl =
-      'https://track.premastex.online/HBDmrk2b?type=inwebviewx&token=CjSJxLhJ8W3hjtjz';
+      'https://track.premastex.online/HBDmrk2b?type=inwebview&token=CjSJxLhJ8W3hjtjz';
   static const String checkerRequestUrl =
       'https://track.premastex.online/HBDmrk2b?type=checker&token=CjSJxLhJ8W3hjtjz';
 
   static final List<Map<String, dynamic>> questions = [
     {
-      "questions": [
+      "questionText":
+          "What is the name of the main character in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Jake Stevenson", "score": 100},
+        {"text": "Sarah Johnson", "score": 0},
+        {"text": "Emily Thompson", "score": 0},
+        {"text": "Michael Rodriguez", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "Where does the story 'Mystery of Gold Tiger' take place?",
+      "answers": [
+        {"text": "Amazon Rainforest", "score": 100},
+        {"text": "Sahara Desert", "score": 0},
+        {"text": "Antarctic Peninsula", "score": 0},
+        {"text": "Himalayan Mountains", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the legendary treasure sought after in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Gold Tiger", "score": 100},
+        {"text": "Diamond Panther", "score": 0},
+        {"text": "Ruby Lion", "score": 0},
+        {"text": "Silver Bear", "score": 0}
+      ]
+    },
+    {
+      "questionText": "Who is the antagonist in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Dr. Anton Richter", "score": 100},
+        {"text": "Professor Samantha Jones", "score": 0},
+        {"text": "Captain William Smith", "score": 0},
+        {"text": "Detective John Anderson", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What profession does the main character Jake Stevenson have in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Archaeologist", "score": 100},
+        {"text": "Astronaut", "score": 0},
+        {"text": "Chef", "score": 0},
+        {"text": "Pilot", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "Who is Jake Stevenson's trusted companion in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Mia Rodriguez", "score": 100},
+        {"text": "Max Johnson", "score": 0},
+        {"text": "Lucy Thompson", "score": 0},
+        {"text": "Ethan Davis", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the lost civilization Jake Stevenson is searching for in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "El Dorado", "score": 100},
+        {"text": "Atlantis", "score": 0},
+        {"text": "Shangri-La", "score": 0},
+        {"text": "Avalon", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the significance of the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "It grants immense wealth and power.", "score": 100},
+        {"text": "It brings eternal youth.", "score": 0},
+        {"text": "It provides wisdom and knowledge.", "score": 0},
+        {"text": "It is a symbol of peace and harmony.", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "Who originally discovered the legend of the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Alexander Stevenson", "score": 100},
+        {"text": "Christopher Johnson", "score": 0},
+        {"text": "Isabella Thompson", "score": 0},
+        {"text": "Daniel Rodriguez", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the secret society trying to obtain the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "The Brotherhood of Shadows", "score": 100},
+        {"text": "The Order of Phoenix", "score": 0},
+        {"text": "The League of Legends", "score": 0},
+        {"text": "The Circle of Secrets", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the jungle guide hired by Jake Stevenson in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Carlos Ramirez", "score": 100},
+        {"text": "Juan Martinez", "score": 0},
+        {"text": "Miguel Hernandez", "score": 0},
+        {"text": "Pedro Gonzalez", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the main obstacle Jake Stevenson faces in his quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Booby-trapped temples", "score": 100},
+        {"text": "Rival treasure hunters", "score": 0},
+        {"text": "Natural disasters", "score": 0},
+        {"text": "Uncharted territories", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the hidden temple where the Gold Tiger is believed to be located in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "Temple of the Jaguar", "score": 100},
+        {"text": "Temple of the Eagle", "score": 0},
+        {"text": "Temple of the Serpent", "score": 0},
+        {"text": "Temple of the Wolf", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "Who is revealed to be the true identity of Dr. Anton Richter in 'Mystery of Gold Tiger'?",
+      "answers": [
         {
-          "questionText":
-              "What is the name of the main character in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Jake Stevenson", "score": 100},
-            {"text": "Sarah Johnson", "score": 0},
-            {"text": "Emily Thompson", "score": 0},
-            {"text": "Michael Rodriguez", "score": 0}
-          ]
+          "text": "The mastermind behind the Brotherhood of Shadows",
+          "score": 100
+        },
+        {"text": "A former colleague of Jake Stevenson", "score": 0},
+        {"text": "An undercover agent for Interpol", "score": 0},
+        {"text": "A descendant of the ancient civilization", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the final fate of the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "It is lost forever.", "score": 100},
+        {"text": "It is destroyed.", "score": 0},
+        {"text": "It is donated to a museum.", "score": 0},
+        {"text": "It is returned to its rightful owners.", "score": 0}
+      ]
+    },
+    {
+      "questionText": "What is the twist ending in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {
+          "text": "The Gold Tiger was a metaphor for inner strength all along.",
+          "score": 100
         },
         {
-          "questionText":
-              "Where does the story 'Mystery of Gold Tiger' take place?",
-          "answers": [
-            {"text": "Amazon Rainforest", "score": 100},
-            {"text": "Sahara Desert", "score": 0},
-            {"text": "Antarctic Peninsula", "score": 0},
-            {"text": "Himalayan Mountains", "score": 0}
-          ]
+          "text": "Jake Stevenson wakes up to realize it was all a dream.",
+          "score": 0
         },
         {
-          "questionText":
-              "What is the name of the legendary treasure sought after in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Gold Tiger", "score": 100},
-            {"text": "Diamond Panther", "score": 0},
-            {"text": "Ruby Lion", "score": 0},
-            {"text": "Silver Bear", "score": 0}
-          ]
+          "text":
+              "The Gold Tiger was a decoy, and the real treasure was friendship.",
+          "score": 0
         },
         {
-          "questionText": "Who is the antagonist in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Dr. Anton Richter", "score": 100},
-            {"text": "Professor Samantha Jones", "score": 0},
-            {"text": "Captain William Smith", "score": 0},
-            {"text": "Detective John Anderson", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What profession does the main character Jake Stevenson have in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Archaeologist", "score": 100},
-            {"text": "Astronaut", "score": 0},
-            {"text": "Chef", "score": 0},
-            {"text": "Pilot", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "Who is Jake Stevenson's trusted companion in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Mia Rodriguez", "score": 100},
-            {"text": "Max Johnson", "score": 0},
-            {"text": "Lucy Thompson", "score": 0},
-            {"text": "Ethan Davis", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the name of the lost civilization Jake Stevenson is searching for in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "El Dorado", "score": 100},
-            {"text": "Atlantis", "score": 0},
-            {"text": "Shangri-La", "score": 0},
-            {"text": "Avalon", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the significance of the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "It grants immense wealth and power.", "score": 100},
-            {"text": "It brings eternal youth.", "score": 0},
-            {"text": "It provides wisdom and knowledge.", "score": 0},
-            {"text": "It is a symbol of peace and harmony.", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "Who originally discovered the legend of the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Alexander Stevenson", "score": 100},
-            {"text": "Christopher Johnson", "score": 0},
-            {"text": "Isabella Thompson", "score": 0},
-            {"text": "Daniel Rodriguez", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the name of the secret society trying to obtain the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "The Brotherhood of Shadows", "score": 100},
-            {"text": "The Order of Phoenix", "score": 0},
-            {"text": "The League of Legends", "score": 0},
-            {"text": "The Circle of Secrets", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the name of the jungle guide hired by Jake Stevenson in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Carlos Ramirez", "score": 100},
-            {"text": "Juan Martinez", "score": 0},
-            {"text": "Miguel Hernandez", "score": 0},
-            {"text": "Pedro Gonzalez", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the main obstacle Jake Stevenson faces in his quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Booby-trapped temples", "score": 100},
-            {"text": "Rival treasure hunters", "score": 0},
-            {"text": "Natural disasters", "score": 0},
-            {"text": "Uncharted territories", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the name of the hidden temple where the Gold Tiger is believed to be located in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "Temple of the Jaguar", "score": 100},
-            {"text": "Temple of the Eagle", "score": 0},
-            {"text": "Temple of the Serpent", "score": 0},
-            {"text": "Temple of the Wolf", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "Who is revealed to be the true identity of Dr. Anton Richter in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {
-              "text": "The mastermind behind the Brotherhood of Shadows",
-              "score": 100
-            },
-            {"text": "A former colleague of Jake Stevenson", "score": 0},
-            {"text": "An undercover agent for Interpol", "score": 0},
-            {"text": "A descendant of the ancient civilization", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the final fate of the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "It is lost forever.", "score": 100},
-            {"text": "It is destroyed.", "score": 0},
-            {"text": "It is donated to a museum.", "score": 0},
-            {"text": "It is returned to its rightful owners.", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the twist ending in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {
-              "text":
-                  "The Gold Tiger was a metaphor for inner strength all along.",
-              "score": 100
-            },
-            {
-              "text": "Jake Stevenson wakes up to realize it was all a dream.",
-              "score": 0
-            },
-            {
-              "text":
-                  "The Gold Tiger was a decoy, and the real treasure was friendship.",
-              "score": 0
-            },
-            {
-              "text":
-                  "Dr. Anton Richter was a double agent working for Jake Stevenson.",
-              "score": 0
-            }
-          ]
-        },
-        {
-          "questionText":
-              "What motivated Jake Stevenson to embark on the quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "To honor his father's legacy", "score": 100},
-            {"text": "To prove his archaeological theories", "score": 0},
-            {"text": "To win a bet with his colleagues", "score": 0},
-            {"text": "To impress Mia Rodriguez", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the name of the ancient civilization that supposedly possessed the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "The Jaguar Empire", "score": 100},
-            {"text": "The Eagle Kingdom", "score": 0},
-            {"text": "The Serpent Dynasty", "score": 0},
-            {"text": "The Wolf Tribe", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What is the significance of the Jaguar symbol in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {
-              "text":
-                  "It represents the power and authority of the ancient civilization.",
-              "score": 100
-            },
-            {"text": "It is a warning sign for treasure hunters.", "score": 0},
-            {"text": "It leads the way to the Gold Tiger.", "score": 0},
-            {"text": "It marks the entrance to the hidden temple.", "score": 0}
-          ]
-        },
-        {
-          "questionText":
-              "What unexpected ally aids Jake Stevenson in his quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
-          "answers": [
-            {"text": "A tribe of indigenous people", "score": 100},
-            {"text": "A rival archaeologist", "score": 0},
-            {"text": "A group of mercenaries", "score": 0},
-            {"text": "A colony of wild animals", "score": 0}
-          ]
+          "text":
+              "Dr. Anton Richter was a double agent working for Jake Stevenson.",
+          "score": 0
         }
+      ]
+    },
+    {
+      "questionText":
+          "What motivated Jake Stevenson to embark on the quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "To honor his father's legacy", "score": 100},
+        {"text": "To prove his archaeological theories", "score": 0},
+        {"text": "To win a bet with his colleagues", "score": 0},
+        {"text": "To impress Mia Rodriguez", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the name of the ancient civilization that supposedly possessed the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "The Jaguar Empire", "score": 100},
+        {"text": "The Eagle Kingdom", "score": 0},
+        {"text": "The Serpent Dynasty", "score": 0},
+        {"text": "The Wolf Tribe", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What is the significance of the Jaguar symbol in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {
+          "text":
+              "It represents the power and authority of the ancient civilization.",
+          "score": 100
+        },
+        {"text": "It is a warning sign for treasure hunters.", "score": 0},
+        {"text": "It leads the way to the Gold Tiger.", "score": 0},
+        {"text": "It marks the entrance to the hidden temple.", "score": 0}
+      ]
+    },
+    {
+      "questionText":
+          "What unexpected ally aids Jake Stevenson in his quest for the Gold Tiger in 'Mystery of Gold Tiger'?",
+      "answers": [
+        {"text": "A tribe of indigenous people", "score": 100},
+        {"text": "A rival archaeologist", "score": 0},
+        {"text": "A group of mercenaries", "score": 0},
+        {"text": "A colony of wild animals", "score": 0}
       ]
     }
   ];
